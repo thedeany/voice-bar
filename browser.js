fbf1e0dd52fb82142cb1524d8ddfa18b171b98a5
Brandon Dean <thedeany@gmail.com>
2017-09-27 11:41:36 -0400

    Add new icon to show when call is initiated/incoming

--- browser.js
+++ browser.js
@@ -1,14 +1,17 @@
 const electron = require('electron')
 const BrowserWindow = electron.remote.BrowserWindow
+const ipc = electron.ipcRenderer
 
 const observer = new MutationObserver(function(mutations, observer) {
     for (let mutation in mutations) {
-        if (mutations[mutation].addedNodes[0].className === 'Cl') {
-            let notification = new Notification('Incoming Call')
+        if (mutations[mutation].addedNodes.length && mutations[mutation].addedNodes[0].className === 'Cl') {
+            let notification = new Notification('Call started')
             notification.onclick = () => {
-                console.log('clicked')
                 BrowserWindow.getAllWindows()[0].show()
             }
+            ipc.send('incoming-call')
+        } else if (mutations[mutation].removedNodes.length && mutations[mutation].removedNodes[0].className === 'Cl') {
+            ipc.send('call-ended')
         }
     }
 })
--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const {
 
 let win
 let tray
+let icon
 
 function createWindow() {
   win = new BrowserWindow({
@@ -40,7 +41,7 @@ function createWindow() {
 }
 
 function createTray() {
-  let icon = nativeImage.createFromDataURL(process.platform === 'darwin' ? trayIcon : trayIconWhite)
+  icon = nativeImage.createFromDataURL(process.platform === 'darwin' ? trayIcon : trayIconWhite)
   tray = new Tray(icon)
   tray.on('click', function (event) {
     toggleWindow()
@@ -90,6 +91,15 @@ ipcMain.on('show-window', () => {
   showWindow()
 })
 
+ipcMain.on('incoming-call', () => {
+  const callIcon = nativeImage.createFromDataURL(trayIconCall)
+  tray.setImage(callIcon)
+})
+
+ipcMain.on('call-ended', () => {
+  tray.setImage(icon)
+})
+
 app.on('ready', () => {
   createWindow()
   win.hide()
@@ -111,3 +121,5 @@ app.on('activate', () => {
 let trayIcon = `data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAATVJREFUSA3tlb1qAkEUhVdNYWUlWKZMZSFE0IeIrWVexj7EJ7QQSRHsBJOQ6P3WOTo7OnpBixQe2Ln/Z+be2dWiuOM/TmBqh/qyZxMedHxX4dWqIQS/9ohc8q+MHPsVT+WP5XepqYfCUZCxL3IVtdhw6A+WMyYPBTzthGvlUJz4FIgxAdBjUQcdDCe83TzCpw5aTnLSdMJLJW0lcCJdEj7pqVTs2ZRByMvp1K4oYETelskHdN0otbxOuMkicnYE6mZnVddzsWrmwarpkr8Pvota3zKGISunEy6/HW2wDgUe4R1RhXNuzBpTermy2Vy6R35QoA6WGE7kxhL7ofpk0QYLDCe8I5rFfO9m0DbItX8udqpmUrKF5SUQY3L7aYG+3tSfs/e/phCmSP8PeBve0qS7fbMJbAHCM3a4T4fXqwAAAABJRU5ErkJggg==`
 
 let trayIconWhite = `data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAQAAABKfvVzAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QA/4ePzL8AAADTSURBVDjLzZQxCsJAEEXfxiA2WgmWllYWFhZewtbSy9iLuYV38DAWIhZinajJtxgEE3Zl04gzsDDL/pk3M7BOtLOk5fufCjIKhCjIviqE1pJQqbdVQj57aCoSYNmo5QK5U1aQApMArGpRCcws78gr8NUZW4WBV1B67oaQ4Oh6BXMWjRP6Jgi12Gmc0AMnEOBqLdajWmcJcI9GqmxKeTRSDgidpMZu/ZuWLrbpWzTS1ZDO0UhHQ9pFI20M6QDwMcgqMNIne6tgnqmQJOXairC7//sEXi8fvZWlvsEjAAAAJXRFWHRkYXRlOmNyZWF0ZQAyMDE3LTA5LTI1VDIyOjQ2OjEyKzAyOjAwok8anAAAACV0RVh0ZGF0ZTptb2RpZnkAMjAxNy0wOS0yNVQyMjo0NjoxMiswMjowMNMSoiAAAAAASUVORK5CYII=`
+
+let trayIconCall = `data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAMAAADXqc3KAAAAWlBMVEX///////8AzAD///8AzAAAzAAAzAD///////////////////////////8AzAAAzAD///////8AzAD///8AzAD///////////////////////8AzAD///8AzABthEU/AAAAHXRSTlMAkvlf/f77La2E1AECtevoNDj1O/Hk0ri0kU34j3oV3C0AAAABYktHRACIBR1IAAAACXBIWXMAAABIAAAASABGyWs+AAAAiElEQVQoz72RWQ7DIAwFXyhJ26yle5Z3/2vGJAjayPlsLUsgjxgDBv4UmSFNtq0eCEuJHIxRHAWcGCofgGcBZQLV6qjJRpY2gTqCUhajqTqgYgIXF5LX5VwE7haSVlTUVPzukVT+xF1TSQ88dm6Fp6by73hpqreAfu+vBiL3e7v93TCPcfrZuGco0B1RRU4rTgAAACV0RVh0ZGF0ZTpjcmVhdGUAMjAxNy0wOS0yNlQwODo1MTozNi0wNDowMDa8V/MAAAAldEVYdGRhdGU6bW9kaWZ5ADIwMTctMDktMjZUMDg6NTE6MzYtMDQ6MDBH4e9PAAAAAElFTkSuQmCC`
